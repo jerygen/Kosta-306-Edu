374a940bd080d19eb09b4bd019f8773cc89bcea4
miyeon <myksphone2001@gmail.com>
2026-02-11 22:34:44 +0900

    ex0210 bank 과제 완료

--- Java/work/step01_Basic/src/ex0210/homework2/Account.java
+++ Java/work/step01_Basic/src/ex0210/homework2/Account.java
@@ -1,13 +1,39 @@
 package ex0210.homework2;
 
 public class Account {
-	String account;
-	String name;
-	int balance;
+	private String accountNo;
+	private String owner;
+	private int balance;
 	
-	public Account(String [] data) {
-		account = data[0];
-		name = data[1];
-		if(balance>0) balance += Integer.parseInt(data[2]); 
+	public Account(String accountNo, String owner, int balance) {
+		this.accountNo = accountNo;
+		this.owner = owner;
+		this.balance = balance;
 	}
+
+	public String getAccountNo() {
+		return accountNo;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public boolean withdraw(int amount) {
+        if (balance >= amount) {
+            balance -= amount;
+            return true;
+        }
+        return false;
+    }
+	
+	
 }
--- Java/work/step01_Basic/src/ex0210/homework2/BankApplication.java
+++ Java/work/step01_Basic/src/ex0210/homework2/BankApplication.java
@@ -3,13 +3,12 @@ package ex0210.homework2;
 import java.util.Scanner;
 
 public class BankApplication {
-	Account [] arr = new Account[100];
-	Account account;
+	private Account [] arr = new Account[100];
 	
 	Scanner sc = new Scanner(System.in);
 	
 	public void printMenu() {
-		//account = new Account();
+		
 		while(true) {
 			System.out.println("------------------------------------------");
 			System.out.println("1.계좌생성 | 2.계좌목록 | 3.예금 | 4.출금 | 5.종료");
@@ -19,10 +18,7 @@ public class BankApplication {
 			int choice = Integer.parseInt(sc.nextLine());
 			switch(choice) {
 			case 1:
-				System.out.println("---------");
-				System.out.println("계좌생성");
-				System.out.println("---------");
-				
+				createAccount();
 				break;
 			case 2:
 				System.out.println("---------");
@@ -48,6 +44,110 @@ public class BankApplication {
 			}
 		}
 	}
+	
+    // 1. 계좌 생성
+    public void createAccount() {
+        System.out.println("------");
+        System.out.println("계좌생성");
+        System.out.println("------");
+
+        System.out.print("계좌번호: ");
+        String accountNo = sc.nextLine();
+
+        System.out.print("계좌주: ");
+        String owner = sc.nextLine();
+
+        System.out.print("초기입금액: ");
+        int balance = Integer.parseInt(sc.nextLine());
+
+        Account newAccount = new Account(accountNo, owner, balance);
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == null) {
+                arr[i] = newAccount;
+                break;
+            }
+        }
+
+        System.out.println("결과: 계좌가 생성되었습니다.");
+    }
+    
+    // 2. 계좌 목록
+    public void accountList() {
+        System.out.println("------");
+        System.out.println("계좌목록");
+        System.out.println("------");
+
+        for (Account account : arr) {
+            if (account != null) {
+                System.out.println(
+                        account.getAccountNo() + "\t" +
+                        account.getOwner() + "\t" +
+                        account.getBalance());
+            }
+        }
+    }
+	
+    // 계좌 찾기
+    public Account findAccount(String accountNo) {
+        for (Account account : arr) {
+            if (account != null) {
+                if (account.getAccountNo().equals(accountNo)) {
+                    return account;
+                }
+            }
+        }
+        return null;
+    }
+    
+    // 3. 예금
+    public void deposit() {
+        System.out.println("------");
+        System.out.println("예금");
+        System.out.println("------");
+
+        System.out.print("계좌번호: ");
+        String accountNo = sc.nextLine();
+
+        System.out.print("예금액: ");
+        int money = Integer.parseInt(sc.nextLine());
+
+        Account account = findAccount(accountNo);
+
+        if (account != null) {
+            account.deposit(money);
+            System.out.println("결과: 예금이 성공되었습니다.");
+        } else {
+            System.out.println("결과: 해당 계좌가 없습니다.");
+        }
+    }
+
+    // 4. 출금
+    public void withdraw() {
+        System.out.println("------");
+        System.out.println("출금");
+        System.out.println("------");
+
+        System.out.print("계좌번호: ");
+        String accountNo = sc.nextLine();
+
+        System.out.print("출금액: ");
+        int money = Integer.parseInt(sc.nextLine());
+
+        Account account = findAccount(accountNo);
+
+        if (account != null) {
+            boolean result = account.withdraw(money);
+            if (result) {
+                System.out.println("결과: 출금이 성공되었습니다.");
+            } else {
+                System.out.println("결과: 잔액이 부족합니다.");
+            }
+        } else {
+            System.out.println("결과: 해당 계좌가 없습니다.");
+        }
+    }
+	
 	
 
 }
