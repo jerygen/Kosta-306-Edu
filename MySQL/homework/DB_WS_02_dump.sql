c668744f3a9eee7eaaeb02a76040c9d959c9e7ef
miyeon <myksphone2001@gmail.com>
2026-03-03 21:50:24 +0900

    ex0303 과제

--- MySQL/homework/DB_WS_02_dump.sql
+++ MySQL/homework/DB_WS_02_dump.sql
@@ -28,6 +28,34 @@ insert into account (account_seq, account_number, balance, user_seq) values (20,
 SELECT * FROM USERS;
 SELECT * FROM ACCOUNT;
 
+-- 1. 고객 번호가 111인 계좌 테이블을 조회한다. 
+select * from account where user_seq = 111;
+-- 2. 잔고가 5000미만인 계좌 테이블을 조회한다.
+select * from account where balance<5000;
+-- 3. 잔고가 5000 이상 10000 이하인 계좌 테이블을 조회한다.
+select * from account where balance between 5000 and 10000;
+-- 4. 게좌번호 중 '4'를 포함하는 계좌 테이블을 조회한다.
+select * from account where account_number like '%4%';
+-- 5. 고객명이 '삼'으로 시작하는 고객 테이블을 조회한다.
+select * from users where name like '삼%';
+-- 6. 계좌 테이블에 있는 고객 번호를 중복없이 조회한다.
+select distinct user_seq from account;
+-- 7. 잔고가 없는 계좌 테이블을 조회한다.
+select * from account where balance is null;
+-- 8. 고객번호가 있는 계좌 테이블을 조회한다.
+select * from account where user_seq is not null;
+-- 9. 고객번호가 있고, 잔고가 4000 이하인 계좌 테이블을 조회한다.
+select * from account where user_seq is not null and balance<=4000;
+-- 10. 계좌 테이블을 고객번호 기준으로 오름차순으로 정렬하여 조회한다.
+select * from account order by user_seq;
+-- 11. 계좌 테이블을 고객번호 기준으로 오름차순으로 그리고 그 안에서 잔고 기준으로 내림차순으로 정렬하여 조회한다.
+select * from account order by user_seq, balance desc;
+-- 12. 계좌 테이블을 조회하되, balance 값이 없으면 0으로 표시한다.
+select account_seq, account_number, ifnull(balance, 0) as balance, user_seq from account;
+-- 13. 고객 테이블을 조회하되, email은 @포함 뒷 부분은 뺴고 앞 아이디만 표시한다. 컬럼 명도 email 대신 email_id로 변경한다.
+select user_seq, name, substring_index(email, '@',1) As email_id from users;
+
+
 
 
 
--- MySQL/homework/DB_WS_02_김미연.sql
+++ MySQL/homework/DB_WS_02_김미연.sql
@@ -7,3 +7,30 @@ create table users(
     is_sleep varchar(5)
 );
 
+alter table users add constraint primary key(user_seq);
+
+alter table users add constraint unique (email);
+alter table users modify email varchar(20) not null;
+
+alter table users alter column is_sleep set default 'N';
+
+desc users;
+
+insert into users(user_seq, name, email, phone) values(111, '홍길동', 'hong@gildong.com','010-1111-1111');
+insert into users(user_seq, name, email, phone) values(222, '이길동', 'lee@gildong.com','010-2222-2222');
+insert into users(user_seq, name, email, phone) values(333, '삼길동', 'sam@gildong.com','010-3333-3333');
+
+update users
+set is_sleep='Y'
+where user_seq = 222;
+
+insert into users(user_seq, name, email, phone) values(222, '이길동2', 'lee2@gildong.com', '010-2222-2222'); -- error: primary key는 중복 값 안 됨
+insert into users(user_seq, name, email, phone) values(2222, '이길동2', 'lee@gildong.com', '010-2222-2222'); -- error: email에는 중복 안 됨
+insert into users(user_seq, name, phone) values(2222, '이길동2', '010-2222-2222'); -- error: email은 기본값이 없고 null이 안 되기 때문에 안 됨
+insert into users(user_seq, name, email, phone) values(2222, '이길동2', 'lee2@gildong.com', '010-2222-2222');
+
+delete from users where user_seq = 2222;
+
+select * from users;
+
+drop table users;
